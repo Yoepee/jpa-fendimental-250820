0369ac92320e7df4142badc2caae34c35332d973
Yoepee <kdy2975@naver.com>
2025-08-20 11:18:28 +0900

    init

---
+++ src/main/java/com/back/JpaFundimental250820Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaFundimental250820Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaFundimental250820Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/JpaFundimental250820ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaFundimental250820ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
