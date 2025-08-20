0e2a764e838a5a179af52784de92149acd9683e6
Yoepee <kdy2975@naver.com>
2025-08-20 12:40:50 +0900

    Repository + Runner 세팅

---
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -0,0 +1,20 @@
+package com.back.global.initData;
+
+import com.back.domain.post.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BaseInitData {
+    @Autowired
+    private PostRepository postRepository;
+
+    @Bean
+    ApplicationRunner baseInitDataApllicationRunner() {
+        return args -> {
+            postRepository.count();
+        };
+    }
+}
