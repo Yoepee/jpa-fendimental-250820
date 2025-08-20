4194a723560eb9b42400939e0090426c6495eb76
Yoepee <kdy2975@naver.com>
2025-08-20 11:48:22 +0900

    entity 세팅

---
+++ src/main/java/com/back/domain/post/entity/Post.java
@@ -0,0 +1,17 @@
+package com.back.domain.post.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id; // INT
+    private String title; // VARCHAR(255)
+    private String content; // TEXT
+}
