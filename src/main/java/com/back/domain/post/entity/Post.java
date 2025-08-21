6790f6a506ef81cf55fee8515c163ab9b7fac653
Yoepee <kdy2975@naver.com>
2025-08-21 10:17:59 +0900

    생성일, 수정일 추가

--- src/main/java/com/back/domain/post/entity/Post.java
+++ src/main/java/com/back/domain/post/entity/Post.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.time.LocalDateTime;
+
 @NoArgsConstructor
 @Getter
 @Setter
@@ -19,9 +21,13 @@ public class Post {
     private String title; // VARCHAR(255)
     @Column(columnDefinition = "TEXT")
     private String content; // TEXT
+    private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
 
     public Post(String title, String content) {
         this.title = title;
         this.content = content;
+        createDate = LocalDateTime.now();
+        modifyDate = LocalDateTime.now();
     }
 }
