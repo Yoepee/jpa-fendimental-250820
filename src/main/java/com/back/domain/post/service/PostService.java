98f54d2ee26e859f86bb9b2b5154405ca9dd150a
Yoepee <kdy2975@naver.com>
2025-08-21 10:27:23 +0900

    서비스 계층 추가

--- src/main/java/com/back/domain/post/service/PostService.java
+++ src/main/java/com/back/domain/post/service/PostService.java
@@ -1,7 +1,24 @@
 package com.back.domain.post.service;
 
+import com.back.domain.post.entity.Post;
+import com.back.domain.post.repository.PostRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PostService {
+    private PostRepository postRepository;
+
+    public void save(Post post) {
+        postRepository.save(post);
+    }
+
+    public Optional<Post> findById(Integer id) {
+        return postRepository.findById(id);
+    }
+
+    public long count() {
+        return postRepository.count();
+    }
 }
--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -1,7 +1,7 @@
 package com.back.global.initData;
 
 import com.back.domain.post.entity.Post;
-import com.back.domain.post.repository.PostRepository;
+import com.back.domain.post.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +12,7 @@ import java.util.Optional;
 @Configuration
 public class BaseInitData {
     @Autowired
-    private PostRepository postRepository;
+    private PostService postService;
 
     @Bean
     ApplicationRunner baseInitDataApllicationRunner() {
@@ -23,14 +23,14 @@ public class BaseInitData {
     }
 
     void work1() {
-        if (postRepository.count() > 0) return;
+        if (postService.count() > 0) return;
 
-        postRepository.save(new Post("제목 1", "내용 1"));
-        postRepository.save(new Post("제목 2", "내용 2"));
+        postService.save(new Post("제목 1", "내용 1"));
+        postService.save(new Post("제목 2", "내용 2"));
     }
 
     void work2() {
-        Optional<Post> opPost = postRepository.findById(1);
+        Optional<Post> opPost = postService.findById(1);
 
         Post post = opPost.get();
         System.out.println(post.toString());
