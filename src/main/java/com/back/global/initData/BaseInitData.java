2f7c025d792945989895a83c8ce9a37c5e7083fd
Yoepee <kdy2975@naver.com>
2025-08-21 10:37:17 +0900

    Autowired를 생성자에서 처리하도록 변경

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -2,7 +2,6 @@ package com.back.global.initData;
 
 import com.back.domain.post.entity.Post;
 import com.back.domain.post.service.PostService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,8 +10,11 @@ import java.util.Optional;
 
 @Configuration
 public class BaseInitData {
-    @Autowired
     private PostService postService;
+    
+    public BaseInitData(PostService postService) {
+        this.postService = postService;
+    }
 
     @Bean
     ApplicationRunner baseInitDataApllicationRunner() {
