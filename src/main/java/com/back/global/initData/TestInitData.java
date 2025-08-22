fbfb79b1bc07e0880f263025a71eef901ddbfebf
Yoepee <kdy2975@naver.com>
2025-08-22 12:24:01 +0900

    테스트 동작 변경(@Profile)

---
+++ src/main/java/com/back/global/initData/TestInitData.java
@@ -0,0 +1,34 @@
+package com.back.global.initData;
+
+import com.back.domain.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
+
+@Profile("test")
+@Configuration
+@RequiredArgsConstructor
+public class TestInitData {
+    @Autowired
+    @Lazy
+    private TestInitData testInitData;
+    private final PostService postService;
+
+    @Bean
+    ApplicationRunner testInitDataApllicationRunner() {
+        return args -> {
+            work1();
+        };
+    }
+
+    public void work1() {
+        if (postService.count() >= 4) return;
+
+        postService.write("제목 3", "내용 3");
+        postService.write("제목 4", "내용 4");
+    }
+}
