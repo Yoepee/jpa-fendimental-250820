9a0d50592cb86d6efd7198f277792ea94fd7a846
Yoepee <kdy2975@naver.com>
2025-08-22 11:36:47 +0900

    테스트 독립성(롤백)

--- src/test/java/com/back/domain/post/repository/PostRepositoryTest.java
+++ src/test/java/com/back/domain/post/repository/PostRepositoryTest.java
@@ -5,10 +5,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
+@Rollback
 public class PostRepositoryTest {
     @Autowired
     private PostRepository postRepository;
@@ -25,9 +29,19 @@ public class PostRepositoryTest {
     @Test
     @DisplayName("글 생성")
     void t2() {
-        Post post = postRepository.save(new Post("제목 3", "내용 3"));
+        Post post = new Post("제목 3", "내용 3");
+        assertThat(post.getId()).isEqualTo(0);
+        postRepository.save(post);
+        assertThat(post.getId()).isGreaterThan(0);
 
         assertThat(post.getTitle()).isEqualTo("제목 3");
         assertThat(post.getContent()).isEqualTo("내용 3");
+    }
+
+    @Test
+    @DisplayName("글 조회")
+    void t3(){
+        long count = postRepository.count();
+        assertThat(count).isEqualTo(2);
     }
 }
